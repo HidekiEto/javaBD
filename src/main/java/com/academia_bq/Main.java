cebed80427e701d5bad9f53196c9d48aad8e6455
Vitor Eto <vitoreto07@gmail.com>
2024-10-15 13:56:32 -0300

    Primeiras alterações no Main

--- src/main/java/com/academia_bq/Main.java
+++ src/main/java/com/academia_bq/Main.java
@@ -1,6 +1,8 @@
 package com.academia_bq;
+import javax.swing.*;
+import java.awt.*;
 
-public class Main {
+public class Main extends JFrame {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
